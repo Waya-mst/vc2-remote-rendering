6603f6b903299c2c684d3ecbbc216b6a21095ba5
Masaru Ohkawara <m.ohkawara@keio.jp>
2023-07-28 10:54:50 +0900

    [assets/glsl] Fix vertex cordinates

--- app/render.py
+++ app/render.py
@@ -80,9 +80,9 @@ class Context:
         vbo = self.context.buffer(
             np.array(
                 [
-                    [0, 0],
-                    [0, 1],
-                    [1, 0],
+                    [-1, -1],
+                    [-1, 1],
+                    [1, -1],
                     [1, 1],
                 ],
                 dtype="f4",
--- assets/glsl/vertex_shader.glsl
+++ assets/glsl/vertex_shader.glsl
@@ -2,6 +2,4 @@
 
 in vec2 position_vertices;
 
-void main() {
-  gl_Position = vec4((position_vertices - 0.5f) * 2.0f, 0.0f, 1.0f);
-}
+void main() { gl_Position = vec4(position_vertices, 0.0f, 1.0f); }
