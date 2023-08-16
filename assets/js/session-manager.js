88f15d23f04838b393189dde151e4dc5e4e99471
Masaru Ohkawara <m.ohkawara@keio.jp>
2023-08-17 08:33:05 +0900

    [assets/js] Listen events only when the websocket connection is open

--- assets/js/session-manager.js
+++ assets/js/session-manager.js
@@ -18,12 +18,14 @@ class SessionManager {
     });
     this.canvas.addEventListener("pointerdown", function (e) {
       e.preventDefault();
+      if (self.websocket.readyState !== 1) return;
       self.mouseButton = e.button;
       self.startX = e.clientX;
       self.startY = e.clientY;
     });
     this.canvas.addEventListener("pointermove", function (e) {
       e.preventDefault();
+      if (self.websocket.readyState !== 1) return;
       switch (self.mouseButton) {
         case 0: // 左ボタンが押下されている状態
           self.theta += (e.clientX - self.startX) * 0.01;
